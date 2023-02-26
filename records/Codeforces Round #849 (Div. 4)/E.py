a816a36e8a033f84952e0fffae6b313b23eecb32
Kenny Chen <kennygchen@yahoo.com>
2023-02-25 23:44:20 -0800

    Finish Codeforces Round #849 (Div. 4)

--- records/Codeforces Round #849 (Div. 4)/E.py
+++ records/Codeforces Round #849 (Div. 4)/E.py
@@ -4,6 +4,7 @@
     Problem | Negatives and Positives
     Time    | 02/05/2023, 23:43:07
 """
+import sys
 
 
 def ii(num=False):
@@ -23,4 +24,22 @@ def gcd(a, b):
 
 
 for _ in range(ii(1)):
-    pass
+    length = ii(1)
+    array = list(map(int, input().split(" ")))
+    negative_count = 0
+    even_sum = 0
+    min = sys.maxsize
+    min_index = 0
+    for i in range(length):
+        even_sum += abs(array[i])
+        if array[i] < 0:
+            negative_count += 1
+
+        if abs(array[i]) < min:
+            min = abs(array[i])
+            min_index = i
+
+    if negative_count % 2 == 0:
+        print(even_sum)
+    else:
+        print(even_sum - min * 2)
